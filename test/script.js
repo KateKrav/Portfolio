2e8449c28cf80c0d40bc73b5370e3a657d61767a
KateKrav <laylovalova@gmail.com>
2020-04-13 17:59:36 +0300

    -Alignment and adaptive page

---
+++ test/script.js
@@ -0,0 +1,16 @@
+   if (10>5){
+      alert("Yes");
+       }
+
+        alert("Привет мир!");
+
+        for(var i = 0; i < 10; i++)
+        if ( i == 5){
+        continue;
+        }
+        {
+        alert(i);}
+
+        function test() {
+        alert ( "Функция тест" );
+        }
