d2670909347a17bc54e13fa2d9307813b9acad10
Alexey <40887676+alllekso@users.noreply.github.com>
2020-04-28 01:31:23 +0300

    add "Load images" function

--- Portfolio/Scripts.js
+++ Portfolio/Scripts.js
@@ -6,3 +6,16 @@ function hide_galery_menu() {
     hide_element("test");
 
 }
+
+let category = "Graphic_design/";
+
+function set_category(select) {
+    category = select;
+}
+
+function load_images() {
+    for (let i = 1; i < 5; i++) {
+        document.write('<div class="preview_art"><a class="graphic_art-box" href="img/'+ category + i + '.jpg"><img src="img/'+ category + i + '.jpg" ></div>');
+    }
+}
+
