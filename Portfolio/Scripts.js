32558310d1301f4ae0203c2fba7e049a54496002
KateKrav <laylovalova@gmail.com>
2020-05-05 23:55:43 +0300

    -add img, link, js.(Need to put a preview on large images)

--- Portfolio/Scripts.js
+++ Portfolio/Scripts.js
@@ -1,5 +1,17 @@
-let graphic_images_amount = 5;
-let water_color_images_amount = 13;
+let graphic_images_amount = 12;
+let water_color_images_amount = 6;
+let oil_paintings_images_amount = 15;
+let personage_images_amount = 5;
+let background_images_amount = 2;
+let illustration_images_amount = 11;
+let logo_images_amount = 3;
+let banner_images_amount = 4;
+let ledingpage_images_amount = 1;
+
+
+
+
+
 
 let category;
 let images_amount;
@@ -16,16 +28,47 @@ function load_images() {
 
 
 function graphic() {
-    localStorage.setItem("category", "Graphic_design/Graphic/");
+    localStorage.setItem("category", "Tradition_art/Graphic/");
     localStorage.setItem("images_amount", graphic_images_amount);
 }
 
 function watercolor() {
-    localStorage.setItem("category", "Graphic_design/WaterColor/");
+    localStorage.setItem("category", "Tradition_art/WaterColor/");
     localStorage.setItem("images_amount", water_color_images_amount);
 }
 
-function digital_art_select() {
-    localStorage.setItem("category", "Graphic_design/");
-    localStorage.setItem("images_amount", digital_art_images_amount);
+function oil_paintings() {
+    localStorage.setItem("category", "Tradition_art/OilPaintings/");
+    localStorage.setItem("images_amount", oil_paintings_images_amount);
+}
+
+function personage() {
+    localStorage.setItem("category", "Digital_art/personage/");
+    localStorage.setItem("images_amount", personage_images_amount);
+}
+
+function background () {
+    localStorage.setItem("category", "Digital_art/background/");
+    localStorage.setItem("images_amount", background_images_amount);
+}
+
+function illustration () {
+    localStorage.setItem("category", "Digital_art/illustration/");
+    localStorage.setItem("images_amount", illustration_images_amount);
+}
+
+
+function logo() {
+    localStorage.setItem("category", "Design/Logo/");
+    localStorage.setItem("images_amount", logo_images_amount);
+}
+
+function banner () {
+    localStorage.setItem("category", "Design/Banner/");
+    localStorage.setItem("images_amount", banner_images_amount);
+}
+
+function ledingpage () {
+    localStorage.setItem("category", "Design/Ledingpage/");
+    localStorage.setItem("images_amount", ledingpage_images_amount);
 }
