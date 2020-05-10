41a33fa8ccfa10424bc38bdf4e2e871da7fe5b64
Alexey <40887676+alllekso@users.noreply.github.com>
2020-05-10 13:58:21 +0300

    refactoring

--- Scripts.js
+++ Scripts.js
@@ -19,31 +19,33 @@ function load_images() {
     category = localStorage.getItem("category");
     images_amount = localStorage.getItem("images_amount");
     for (let i = 1; i <= images_amount; i++) {
-        document.write(`<div class="preview_art"><a onclick=localStorage.setItem("item_name",${i}) href="img_details.html"><img src="img/${category}${i}.jpg"></div>`);
+        document.write(`<div class="preview_art"><a onclick=localStorage.setItem("item_name",${i}) href="img_details.html"><img src="../../img/${category}${i}.jpg"></div>`);
     }
 }
 
 
 function load_img_details() {
     document.write(`     <div>
-         <a href="#"><img src="img/${localStorage.getItem("category")}/Full/${localStorage.getItem("item_name")}.jpg" alt="img" style="width: 1500px"></a>
+         <a href="#"><img src="../../img/${localStorage.getItem("category")}/Full/${localStorage.getItem("item_name")}.jpg" alt="img" style="width: 1500px"></a>
      </div>
      <div class="text_img">
-         <div class="gradient_teaser">
-             <span>${image_name}</span>
-         </div>
-         <span class="text_img_span_padding">${image_description}</span>
+
          <button class="button_menu">
-             <a href="Image_list.html">
+             <a href="Images_list.html">
                  <img alt="arrow_icon" src="https://img.icons8.com/material-rounded/48/ffffff/back.png"/>
              </a>
          </button>`)
 }
 
+
+// <div class="gradient_teaser">
+//     <span>${image_name}</span>
+//     </div>
+//     <span class="text_img_span_padding">${image_description}</span>
+
 async function read_image_description() {
     let say = await import('img/Tradition_art/Graphic/Full/say.js');
     say.hi(); // Привет!
-
 }
 
 
